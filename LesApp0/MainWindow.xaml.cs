d015693887334127524ad1212049af0dc092c5d0
Bogdan <40561032+BogdanPinchuk@users.noreply.github.com>
2019-07-23 14:39:17 +0300

    Добавьте файлы проекта.

---
+++ LesApp0/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LesApp0
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Виконання дії копіювання інформації між textbox
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Копіювання даних із текстбокса де данні вводяться
+            // і внесення в текстбокс де вони відображуються
+            tbOutput.Text = tbInput.Text;
+        }
+    }
+}
